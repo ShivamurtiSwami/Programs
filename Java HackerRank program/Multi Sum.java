251befecf47a4d32741d7ced4a5014a7e27063d1
Shivamurti Swami <106366744+ShivamurtiSwami@users.noreply.github.com>
2023-10-15 21:49:26 +0530

    Update Multi Sum.java

--- Java HackerRank program/Multi Sum.java
+++ Java HackerRank program/Multi Sum.java
@@ -1,1 +1,20 @@
+class Arithmetic {
+    public Integer sum(Integer[] arr){
+        int total =0;
+        for(int i=0;i<arr.length;i++){
+            total+=arr[i];
+        }
+        return total;
+    }
+    public String sum(String[] arr){
+        String s="";
+        for(int i=0;i<arr.length;i++){
+            s=s.concat(arr[i]);
+        }
+        return s;
+    }
+    
+}
 
+multi sum solution
+  
